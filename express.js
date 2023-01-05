d80c43e3f802880b4dec488518014a23e227c9c6
tgldrchn <tuguldur9190@gmail.com>
2023-01-05 17:08:27 +0800

    env

--- express.js
+++ express.js
@@ -4,8 +4,9 @@ import cors from "cors";
 import router from "./router/post.js";
 import routerTwo from "./router/user.js";
 import dotenv from "dotenv";
-const app = express();
+
 dotenv.config();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
